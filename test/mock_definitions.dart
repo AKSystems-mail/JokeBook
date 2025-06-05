f25f865d497108344622632797dacb5df57dd998
aksystems.mail <aksystems.mail@gmail.com>
2025-06-05 03:09:12 +0000

    Test removed

--- test/mock_definitions.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mockito/annotations.dart';
-import 'package:YourAppName/services/firestore_service.dart'; // Corrected package name
-import 'package:just_audio/just_audio.dart';
-
-part 'mock_definitions.mocks.dart'; // Added part directive
-
-@GenerateMocks([FirestoreService, AudioPlayer])
-void main() {} // main function is often kept empty or used for other test setup
